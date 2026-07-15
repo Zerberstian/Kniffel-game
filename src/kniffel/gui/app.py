289402b6f975d92acbc765c90215580429fc4dc9
Zerberstian <zerberstian681@gmail.com>
2026-07-15 08:42:41 +0200

    created placeholder for ratio in GUI

---
+++ src/kniffel/gui/app.py
@@ -0,0 +1,24 @@
+import tkinter 
+
+root = tkinter.Tk()
+
+root.title("Kniffel Game")
+
+root.geometry("1000x600")  # Set the window size 
+placeholder_Zettel = tkinter.Frame(root, width=400, height=600)
+placeholder_Zettel.configure(bg="lightgray")  # Set the background color of the frame
+placeholder_Zettel.place(x=600, y=0)  # Position the frame at the top-left corner
+
+placeholder_regeln = tkinter.Frame(root, width=600, height=300)
+placeholder_regeln.configure(bg="lightblue")  # Set the background color of the frame
+placeholder_regeln.place(x=0, y=300)  # Position the frame at the top-left corner
+
+placeholder_spielbereich = tkinter.Frame(root, width=600, height=300)
+placeholder_spielbereich.configure(bg="lightyellow")  # Set the background color of the frame
+placeholder_spielbereich.place(x=0, y=0)  # Position the frame at the top-left corner
+
+placeholder_reroll_btw = tkinter.Frame(root, width=80, height=40)
+placeholder_reroll_btw.configure(bg="lightgreen")  # Set the background color of the frame
+placeholder_reroll_btw.place(x=520, y=260)  # Position the frame at the top-left corner
+
+root.mainloop()
