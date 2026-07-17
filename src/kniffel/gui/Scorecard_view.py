32000ff3955f05a585412e86250817a15c064d0b
Zerberstian <zerberstian681@gmail.com>
2026-07-17 12:21:55 +0200

    refactor: update ScoreFame class to inherit from tk.Frame and streamline initialization

--- src/kniffel/gui/Scorecard_view.py
+++ src/kniffel/gui/Scorecard_view.py
@@ -1,12 +1,16 @@
 import tkinter as tk
+from typing import List
 
 NUMBER_OF_CATEGORIES = 16
 
-class ScoreFame:
-    def __init__(self, masterFrame):
-        self.masterFrame = masterFrame
-        self
-        pass
+
+class ScoreFame(tk.Frame): 
+    def __init__(self, master: tk.Frame):
+
+        self.master = master
+        #self.CreatedFrames: List[tk.Frame] = [] 
+        super().__init__(master)
+
 
     def createLabel():
         pass
@@ -14,16 +18,25 @@ class ScoreFame:
     def createButton():
         pass
     
-    def create():
-        for i in range(NUMBER_OF_CATEGORIES):
-            #CreatedFrames[]
-            print(i)
-
-        '''
-        createLabel()
-        createButton()
-        '''
-        pass
 
 if __name__ == "__main__":
-    ScoreFame.create()
+    root = tk.Tk()
+
+    frame1 = tk.Frame(root)
+    frame1.configure(bg="#e62929")
+    frame1.pack(fill="both", expand=True)
+
+    CreatedFrames: List[tk.Frame] = [] 
+
+    for i in range(NUMBER_OF_CATEGORIES):
+        frame = ScoreFame(frame1)
+        frame.pack(side="top")
+        CreatedFrames.append(frame)
+        print(i)
+
+
+    #scoreframe = ScoreFame(frame1).pack()
+
+    print(CreatedFrames)
+
+    root.mainloop()
