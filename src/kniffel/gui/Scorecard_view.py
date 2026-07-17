b8da7ef935d565a1419d6c416971c586b7ac3603
Zerberstian <zerberstian681@gmail.com>
2026-07-17 11:01:00 +0200

    refactor: restructure ScoreFame class and add placeholder methods for label and button creation

--- src/kniffel/gui/Scorecard_view.py
+++ src/kniffel/gui/Scorecard_view.py
@@ -1,10 +1,29 @@
 import tkinter as tk
 
+NUMBER_OF_CATEGORIES = 16
+
 class ScoreFame:
     def __init__(self, masterFrame):
         self.masterFrame = masterFrame
-        
+        self
         pass
 
-    def create():
+    def createLabel():
         pass
+
+    def createButton():
+        pass
+    
+    def create():
+        for i in range(NUMBER_OF_CATEGORIES):
+            #CreatedFrames[]
+            print(i)
+
+        '''
+        createLabel()
+        createButton()
+        '''
+        pass
+
+if __name__ == "__main__":
+    ScoreFame.create()
