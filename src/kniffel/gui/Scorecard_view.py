58fa13569169f13b601602ee29d2e823e190e4a9
Zerberstian <zerberstian681@gmail.com>
2026-07-20 13:37:54 +0200

    refactor: used comments to improve readability

--- src/kniffel/gui/Scorecard_view.py
+++ src/kniffel/gui/Scorecard_view.py
@@ -1,23 +1,24 @@
 import tkinter as tk
 from typing import List
 
-Label_Names  =  ["Einer:", 
-                "Zweier:",
-                "Dreier:", 
-                "Vierer:", 
-                "Fünfer:", 
-                "Sechser:", 
-                "Bonus Oben:",
-                "Dreierpasch:",
-                "Viererpasch:",
-                "Full House:",
-                "Kleine Straße:",
-                "Große Straße:",
-                "Kniffel:",
-                "Chance:",
-                "Bonus Unten:",
-                "Ergebnis:"]
+Score_Lable_name_List  =  ["Einer:", 
+                            "Zweier:",
+                            "Dreier:", 
+                            "Vierer:", 
+                            "Fünfer:", 
+                            "Sechser:", 
+                            "Bonus Oben:",
+                            "Dreierpasch:",
+                            "Viererpasch:",
+                            "Full House:",
+                            "Kleine Straße:",
+                            "Große Straße:",
+                            "Kniffel:",
+                            "Chance:",
+                            "Bonus Unten:",
+                            "Ergebnis:"]
 
+'''Constants for the File'''
 NUMBER_OF_CATEGORIES = 16
 FONTSTYLE = "Times New Roman"
 FONTSIZE = 20
@@ -26,6 +27,7 @@ DICE_IPADX = 30
 DICE_IPADY = 20
 DICE_BG = "#ffffff"
 
+'''Class with function call in init to use function on object creation'''
 class ScoreFrame(tk.Frame): 
     def __init__(self, master: tk.Frame, i):
         super().__init__(master)
@@ -39,14 +41,14 @@ class ScoreFrame(tk.Frame):
         self.createLabel()
         self.createButton()
 
-
+    '''creates Label for each Category in Score_Lable_name_List'''
     def createLabel(self):
         self.CategoryLabel = tk.Label(self)
-        self.CategoryLabel.configure(text = Label_Names[self.index], font=(FONTSTYLE, FONTSIZE), bg=DICE_BG)    # The Label has the same bg color as the Bottons
+        self.CategoryLabel.configure(text = Score_Lable_name_List[self.index], font=(FONTSTYLE, FONTSIZE), bg=DICE_BG)    # The Label has the same bg color as the Bottons
         self.CategoryLabel.grid(row=0, column=0, sticky="nsew")
         
 
-#  Placeholder aktuell für Buttons
+    '''creates a empty Button for each Category'''
     def createButton(self):
         self.CategoryButton = tk.Button(self)
         self.CategoryButton.configure(text = "", font=(FONTSTYLE, FONTSIZE), bg=DICE_BG)
--- src/kniffel/gui/app.py
+++ src/kniffel/gui/app.py
@@ -2,23 +2,24 @@ import tkinter as tk
 from Scorecard_view import ScoreFrame
 from typing import List
 
-Label_Names  =  ["Einer:", 
-                "Zweier:",
-                "Dreier:", 
-                "Vierer:", 
-                "Fünfer:", 
-                "Sechser:", 
-                "Bonus Oben:",
-                "Dreierpasch:",
-                "Viererpasch:",
-                "Full House:",
-                "Kleine Straße:",
-                "Große Straße:",
-                "Kniffel:",
-                "Chance:",
-                "Bonus Unten:",
-                "Ergebnis:"]
+Score_Lable_name_List  =  ["Einer:", 
+                            "Zweier:",
+                            "Dreier:", 
+                            "Vierer:", 
+                            "Fünfer:", 
+                            "Sechser:", 
+                            "Bonus Oben:",
+                            "Dreierpasch:",
+                            "Viererpasch:",
+                            "Full House:",
+                            "Kleine Straße:",
+                            "Große Straße:",
+                            "Kniffel:",
+                            "Chance:",
+                            "Bonus Unten:",
+                            "Ergebnis:"]
 
+'''Constants for the File'''
 NUMBER_OF_CATEGORIES = 16
 FONTSTYLE = "Times New Roman"
 FONTSIZE = 20
@@ -27,9 +28,11 @@ DICE_IPADX = 30
 DICE_IPADY = 20
 DICE_BG = "#ffffff"
 
+'''Import for Rules (.txt File)'''
 with open("Kniffel-game/src/kniffel/gui/regeln.txt", "r", encoding="utf-8") as file:
     rules_text = file.read()
 
+'''Base for GUI (Window)'''
 root = tk.Tk()
 root.title("Kniffel Game")
 root.maxsize(width=1920, height=1080)       # 1920x1080
@@ -37,57 +40,22 @@ root.minsize(width=900, height=600)         # 900x600
 root.geometry("1600x900")                   # Set the window size 
 root.configure(bg="green")
 
-# Weight of the Root-grid
+'''Weight of the Root-grid'''
 root.grid_columnconfigure(0, weight=1)      
 root.grid_columnconfigure(1, weight=1)
 root.grid_rowconfigure(0, weight=2)
 root.grid_rowconfigure(1, weight=2)
 
+'''Area for Kniffel Points'''
 Scorecard_Area = tk.Frame(root)
-Scorecard_Area.configure(bg="lightgray")  # Set the background color of the frame
-Scorecard_Area.grid(row=0 ,column=1, rowspan=2, sticky="nsew")  # Position the frame at the top-left corner
+Scorecard_Area.configure(bg="lightgray")                            # Set the background color of the frame
+Scorecard_Area.grid(row=0 ,column=1, rowspan=2, sticky="nsew")      # Position the frame at the top-left corner
 
-Scorecard_Area.grid_columnconfigure(0, weight=1)
-Scorecard_Area.grid_columnconfigure(1, weight=1)
-
+'''PLayarea'''
 Dice_Playarea = tk.Frame(root)
-Dice_Playarea.configure(bg="#499240")  # Set the background color of the frame
-Dice_Playarea.grid(column=0, row=0, sticky="nsew") # Position the frame at the top-left corner
-
+Dice_Playarea.configure(bg="#499240")                             # Set the background color of the frame
+Dice_Playarea.grid(column=0, row=0, sticky="nsew")                  # Position the frame at the top-left corner
 Dice_Playarea.grid_rowconfigure(0, minsize=200)
-
-Dice_Button_List = []
-
-
-
-GUI_Dice_1 = tk.Button(Dice_Playarea)
-GUI_Dice_1.grid(column=0, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)#, sticky="nsew")
-GUI_Dice_1.configure(bg= DICE_BG, text=f"")
-Dice_Button_List.append(GUI_Dice_1)
-
-GUI_Dice_2 = tk.Button(Dice_Playarea)
-GUI_Dice_2.grid(column=1, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)#, sticky="nsew")
-GUI_Dice_2.configure(bg= DICE_BG, text=f"")
-Dice_Button_List.append(GUI_Dice_2)
-
-GUI_Dice_3 = tk.Button(Dice_Playarea)
-GUI_Dice_3.grid(column=2, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)#, sticky="nsew")
-GUI_Dice_3.configure(bg= DICE_BG, text=f"")
-Dice_Button_List.append(GUI_Dice_3)
-
-GUI_Dice_4 = tk.Button(Dice_Playarea)
-GUI_Dice_4.grid(column=3, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)#, sticky="nsew")
-GUI_Dice_4.configure(bg=DICE_BG, text=f"")
-Dice_Button_List.append(GUI_Dice_4)
-
-GUI_Dice_5 = tk.Button(Dice_Playarea)
-GUI_Dice_5.grid(column=4, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)#, sticky="nsew")
-GUI_Dice_5.configure(bg= DICE_BG, text=f"")
-Dice_Button_List.append(GUI_Dice_5)
-
-Reroll_Button = tk.Button(Dice_Playarea)
-Reroll_Button.grid(column=5, row=1, ipadx=10, ipady=10)#, sticky="nsew")
-Reroll_Button.configure(bg= DICE_BG, text=f"roll", font=(FONTSTYLE, FONTSIZE))
 
 Dice_Playarea.grid_columnconfigure(0, weight=1)
 Dice_Playarea.grid_columnconfigure(1, weight=1)
@@ -96,13 +64,49 @@ Dice_Playarea.grid_columnconfigure(3, weight=1)
 Dice_Playarea.grid_columnconfigure(4, weight=1)
 Dice_Playarea.grid_rowconfigure(0, weight=2)
 
+'''List with all GUI Elements for Dice'''
+'''can or will be used for sorting, instad of sorting in Dice itself'''
+Dice_Button_List = []
 
-Rule_Widget = tk.Text(root)
-Rule_Widget.grid(column=0, row=1, sticky="nsew")
-Rule_Widget.configure(font=(FONTSTYLE, FONTSIZESMOL))
-Rule_Widget.insert(tk.END, rules_text)
+'''Buttons for Dice (GUI Element)'''
+GUI_Dice_1 = tk.Button(Dice_Playarea)
+GUI_Dice_1.grid(column=0, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)
+GUI_Dice_1.configure(bg= DICE_BG, text=f"")
+Dice_Button_List.append(GUI_Dice_1)
+
+GUI_Dice_2 = tk.Button(Dice_Playarea)
+GUI_Dice_2.grid(column=1, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)
+Dice_Button_List.append(GUI_Dice_2)
+
+GUI_Dice_3 = tk.Button(Dice_Playarea)
+GUI_Dice_3.grid(column=2, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)
+GUI_Dice_3.configure(bg= DICE_BG, text=f"")
+Dice_Button_List.append(GUI_Dice_3)
+
+GUI_Dice_4 = tk.Button(Dice_Playarea)
+GUI_Dice_4.grid(column=3, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)
+GUI_Dice_4.configure(bg=DICE_BG, text=f"")
+Dice_Button_List.append(GUI_Dice_4)
+
+GUI_Dice_5 = tk.Button(Dice_Playarea)
+GUI_Dice_5.grid(column=4, row=0, ipadx=DICE_IPADX, ipady=DICE_IPADY)
+GUI_Dice_5.configure(bg= DICE_BG, text=f"")
+Dice_Button_List.append(GUI_Dice_5)
 
 
+Reroll_Button = tk.Button(Dice_Playarea)
+Reroll_Button.grid(column=5, row=1, ipadx=10, ipady=10)
+Reroll_Button.configure(bg= DICE_BG, text=f"roll", font=(FONTSTYLE, FONTSIZE))
+
+
+'''Textbox for imported Rule File'''
+Rule_Text_Widget = tk.Text(root)
+Rule_Text_Widget.grid(column=0, row=1, sticky="nsew")
+Rule_Text_Widget.configure(font=(FONTSTYLE, FONTSIZESMOL))
+Rule_Text_Widget.insert(tk.END, rules_text)
+
+
+'''creates score subframes, one per category with Label and Button'''
 CreatedFrames: List[tk.Frame] = [] 
 
 for i in range(NUMBER_OF_CATEGORIES):
